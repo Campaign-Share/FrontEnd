821a79d82b8a6bbc83058f0171cab5ce70a30c44
parkjaehoon <yukihoon12@naver.com>
2020-10-27 23:06:26 +0900

    [CREATE]폴더구조

--- src/Routing.js
+++ src/Routing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Mypage from './components/Profile/mypage/Mypage';
 
 const Routing = () => {
 	return (
 		<Switch>
-			<Route exact path="/" render={() => <IntroduceContainer />} />
+			{/* <Route exact path="/" render={() => <IntroduceContainer />} /> */}
+			<Route exact path="/Mypage" component={Mypage} />
 		</Switch>
 	);
 };
---
+++ src/components/Navigation/sideBar/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const SideBox = styled.div`
+	height: 100vh;
+	width: 300px;
+	background-color: ;
+`;
