f54b7d56afbe7b54ccd79f73e526be0431c4e107
Haerin <kim34458747@gmail.com>
2020-10-27 19:50:24 +0900

    App setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
-	return <div>hello</div>;
+	return (
+		<Router>
+			<GlobalStyle />
+			<Switch>
+				<Route exact path="/" render={() => (<IntroduceContainer/>)} />
+			</Switch>
+		</Router>
+	);
 };
-
 export default App;
