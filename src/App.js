6c578338f80fac2e02326a9da2380b968e9c5214
Haerin <kim34458747@gmail.com>
2020-10-27 22:01:19 +0900

    folder setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import Routing from './Routing.js'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Routing from './Routing.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<Router>
 			<GlobalStyle />
-			<Routing/>
+			<Routing />
 		</Router>
 	);
 };
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/Routing.js
+++ src/Routing.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-const Routing = ()=>{
-    return(
-        <Switch>
-			<Route exact path="/" render={() => (<IntroduceContainer/>)} />
+import { Route, Switch } from 'react-router-dom';
+
+const Routing = () => {
+	return (
+		<Switch>
+			<Route exact path="/" render={() => <IntroduceContainer />} />
 		</Switch>
-    )
-}
+	);
+};
+
 export default Routing;
