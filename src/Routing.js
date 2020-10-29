b89b3a3eacc2bac0ae6d99626f38415122773bae
parkjaehoon <yukihoon12@naver.com>
2020-10-29 13:11:48 +0900

    [CREATE] 마이페이지 퍼블리싱

--- src/Routing.js
+++ src/Routing.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Mypage from './components/Profile/mypage/Mypage';
-
+import EditProfile from './components/Profile/editProfile/EditProfile.js'
 const Routing = () => {
 	return (
 		<Switch>
 			{/* <Route exact path="/" render={() => <IntroduceContainer />} /> */}
 			<Route exact path="/Mypage" component={Mypage} />
+			<Route exact path='/EditProfile' component={EditProfile}/>
 		</Switch>
 	);
 };
