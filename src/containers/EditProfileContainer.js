732b9c6ccd8dcccc0a311ecbb25221e3db52dbc6
parkjaehoon <yukihoon12@naver.com>
2020-11-01 23:30:05 +0900

    [CREATE] mypage, EditProfile 컨테이너 생성

---
+++ src/containers/EditProfileContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import EditProfile from '../components/Profile/editProfile/EditProfile';
+
+const EditProfileContainer = () => {
+	return <EditProfile />;
+};
+export default EditProfile;
