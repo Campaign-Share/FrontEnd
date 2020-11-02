6dbd836483fcdff7b317566ca305b77908c6bc08
Haerin <kim34458747@gmail.com>
2020-11-02 16:06:47 +0900

    redux setting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import rootReducers from './modules';
+import {createStore} from 'redux';
+import {provider} from 'react-redux';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
