98cc682d16f16d6d8a8230bacbc4e9c471f011d7
parkjaehoon <yukihoon12@naver.com>
2020-11-02 16:17:26 +0900

    [MODIFY] Provider 철자 수정

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import {createStore} from 'redux';
-import {provider} from 'react-redux';
+import {Provider} from 'react-redux';
 import rootReducer from "./modules";
 
 const store = createStore(rootReducer);
-ReactDOM.render(<provider store={store}><App /></provider>, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
