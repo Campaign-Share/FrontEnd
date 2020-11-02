52f74bb9f53290c022b6679f2da54cfe12d01db9
Haerin <kim34458747@gmail.com>
2020-11-02 15:32:47 +0900

    folder 수정

--- src/containers/LoginContainer/LoginContainer.js
+++ src/containers/LoginContainer/LoginContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from '../../components/User/Login/Login';
 
 const LoginContainer = () => {
-	return <Login />;
+	return (<Login />);
 };
 
 export default LoginContainer;
--- src/containers/SignUpContainer/SignUpContainer.js
+++ src/containers/SignUpContainer/SignUpContainer.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Auth from '../../components/User/SignUp/Auth';
 import Email from '../../components/User/SignUp/Email';
 import SignUp from '../../components/User/SignUp/SignUp';
 
 const SignUpContainer = (props) => {
-	const url = props.match.params.email;
+	const url = props.match.params;
+	console.log(props.match.url);
 	let component;
 	if (url === 'email') {
 		component = <Email />;
+	} else if(props.match.url === '/signUp/auth'){
+		component = <Auth />;
 	} else {
-		component = <SignUp />;
+		component = <SignUp/>
 	}
-	console.log(url);
 
 	return <React.Fragment>{component}</React.Fragment>;
 };
