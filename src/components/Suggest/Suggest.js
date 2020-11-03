82530144eeeb2a1b8bf96a13d090fd9fc2d7ae76
Wanseo <sonwanseo@gmail.com>
2020-11-03 17:01:34 +0900

    Publishing Suggest Page

---
+++ src/components/Suggest/Suggest.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as S from './style';
+import SideBar from '../Navigation/sideBar/SideBar';
+import { profile } from '../../assets/img';
+
+const Suggest = () => {
+	return (
+		<S.MainWrapper>
+			<SideBar />
+			<S.RightWrapper>
+				<S.Header>
+					<S.HeaderIcon src={profile} />
+				</S.Header>
+				<S.BodyWrapper>
+					<S.SuggestWrapper>
+						<S.SuggestTitle />
+						<S.SuggestBodyWrapper>
+							<S.SuggestLeftWrapper>
+								<S.SuggestImageInput>이미지를 넣어주세요</S.SuggestImageInput>
+							</S.SuggestLeftWrapper>
+							<S.SuggestDividingLine />
+							<S.SuggestRightWrapper>
+								<S.SuggestRightTitle>
+									캠페인 설명 & 참여 방법
+								</S.SuggestRightTitle>
+								<S.SuggestRightInput>
+									캠페인에 대한 것을 적어주세요.
+								</S.SuggestRightInput>
+							</S.SuggestRightWrapper>
+						</S.SuggestBodyWrapper>
+						<S.SuggestBottomWrapper>
+							<S.SuggestSubmitBtn>등록</S.SuggestSubmitBtn>
+						</S.SuggestBottomWrapper>
+					</S.SuggestWrapper>
+				</S.BodyWrapper>
+			</S.RightWrapper>
+		</S.MainWrapper>
+	);
+};
+
+export default Suggest;
