fdf4f678a8d6660d0c82e73fad528dfe84505470
Wanseo <sonwanseo@gmail.com>
2020-11-04 20:20:18 +0900

    Introduce Page px to rem

--- src/components/Introduce/style.js
+++ src/components/Introduce/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const MainWrapper = styled.div`
 	background-image: url(${(props) => props.url});
 	background-position: left top;
-	background-size: 100% 700px;
+	background-size: 100% 43.75rem;
 	background-repeat: no-repeat;
 	flex: 1;
 	height: 100%;
@@ -11,36 +11,36 @@ export const MainWrapper = styled.div`
 
 export const TopWrapper = styled.div`
 	width: 100%;
-	height: 100px;
+	height: 6.25rem;
 `;
 
 export const LogoWrapper = styled.div`
-	margin-left: 15px;
-	margin-top: 15px;
+	margin-left: 0.9375rem;
+	margin-top: 0.9375rem;
 	display: inline-block;
 `;
 
 export const Logo = styled.img`
-	width: 70px;
+	width: 4.375rem;
 `;
 
 export const LinkWrapper = styled.div`
 	float: right;
-	margin-top: 20px;
-	margin-right: 80px;
+	margin-top: 1.25rem;
+	margin-right: 5rem;
 `;
 
 export const Link = styled.a`
-	margin-left: 100px;
+	margin-left: 6.25rem;
 	font-family: Noto Sans CJK KR;
 	color: white;
-	font-size: 23px;
+	font-size: 1.438rem;
 	text-decoration: none;
 `;
 
 export const BodyWrapper = styled.div`
 	width: 90%;
-	height: calc(100% - 100px);
+	height: calc(100% - 6.25rem);
 	display: flex;
 	justify-content: center;
 	margin-left: 5%;
@@ -48,7 +48,7 @@ export const BodyWrapper = styled.div`
 `;
 
 export const TextWrapper = styled.div`
-	width: calc(100% - 750px);
+	width: calc(100% - 46.88rem);
 	height: 80%;
 	display: flex;
 	flex-direction: column;
@@ -56,32 +56,32 @@ export const TextWrapper = styled.div`
 `;
 
 export const TextTitle = styled.h1`
-	width: 605px;
-	height: 70px;
+	width: 37.81rem;
+	height: 4.375rem;
 	font-family: Noto Sans CJK KR;
 	margin: 0;
-	font-size: 50px;
+	font-size: 3.125rem;
 	display: block;
 	color: #535353;
 `;
 
 export const TextDividingLine = styled.div`
-	width: 620px;
-	height: 3px;
+	width: 38.75rem;
+	height: 0.1875rem;
 	background-color: #707070;
 	display: block;
 `;
 
 export const TextExplanation = styled.p`
-	width: 605px;
-	height: 100px;
-	font-size: 17px;
+	width: 37.81rem;
+	height: 6.25rem;
+	font-size: 1.063rem;
 	font-family: Noto Sans CJK KR;
 	color: #535353;
 `;
 
 export const RightBackgroundImg = styled.img`
-	width: 750px;
-	height: 600px;
+	width: 46.88rem;
+	height: 37.5rem;
 	display: inline;
 `;
