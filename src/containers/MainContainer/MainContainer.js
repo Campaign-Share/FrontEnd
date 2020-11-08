a71135198fa3ed91198a057abb7d0c50fb6dd005
Wanseo <sonwanseo@gmail.com>
2020-11-08 22:53:23 +0900

    Add Suggest Error Handling Code

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -14,12 +14,14 @@ const MainContainer = () => {
 	});
 	const [posterImg, setPosterImg] = useState();
 	const [isModal, setIsModal] = useState(false);
+	const [isError, setIsError] = useState(false);
 
 	const history = useHistory();
 
 	const checkPeriod = () => {
 		if (suggestForm.periodDay > 30) {
 			alert('기간은 30일을 초과할 수 없습니다.');
+			setIsError(true);
 		}
 	};
 
@@ -27,42 +29,46 @@ const MainContainer = () => {
 		for (var key in suggestForm) {
 			if (suggestForm[key].length === 0) {
 				alert('빈 칸을 채워주세요');
+				setIsError(true);
 				break;
 			}
 		}
 	};
 
 	const onSubmitForm = async () => {
+		setIsError(false);
 		checkPeriod();
 		onClickBtn();
-		var formData = new FormData();
-		formData.append('title', suggestForm.title);
-		formData.append('subTitle', suggestForm.subTitle);
-		formData.append('poster', suggestForm.poster);
-		formData.append('introduction', suggestForm.introduction);
-		formData.append('participation', suggestForm.participation);
-		formData.append('periodDay', suggestForm.periodDay);
-		try {
-			const res = await requestApiWithAccessToken(
-				'/v1/campaigns',
-				formData,
-				{},
-				'post',
-			);
-			// if (res.data.status == '400') {
-			// 	alert()
-			// }
-			if (res.data.status == '401') {
-				// history.push('/login');
-				return;
+		if (isError === false) {
+			var formData = new FormData();
+			formData.append('title', suggestForm.title);
+			formData.append('subTitle', suggestForm.subTitle);
+			formData.append('poster', suggestForm.poster);
+			formData.append('introduction', suggestForm.introduction);
+			formData.append('participation', suggestForm.participation);
+			formData.append('periodDay', suggestForm.periodDay);
+			try {
+				const res = await requestApiWithAccessToken(
+					'/v1/campaigns',
+					formData,
+					{},
+					'post',
+				);
+				// if (res.data.status == '400') {
+				// 	alert()
+				// }
+				if (res.data.status == '401') {
+					// history.push('/login');
+					return;
+				}
+				if (res.data.status == '409') {
+					alert('하루 생성 가능 캠페인을 초과했습니다.');
+					return;
+				}
+				setIsModal(true);
+			} catch (error) {
+				console.log(error);
 			}
-			if (res.data.status == '409') {
-				alert('하루 생성 가능 캠페인을 초과했습니다.');
-				return;
-			}
-			setIsModal(true);
-		} catch (error) {
-			console.log(error);
 		}
 	};
 
