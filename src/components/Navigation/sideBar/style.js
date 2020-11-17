9e6bd41500ab5b958d19becbffd9c9a64523ae06
Haerin <kim34458747@gmail.com>
2020-11-18 01:16:30 +0900

    sideBar style 수정

--- src/components/Navigation/sideBar/style.js
+++ src/components/Navigation/sideBar/style.js
@@ -1,21 +1,24 @@
 import styled from 'styled-components';
 
 export const SideBox = styled.div`
-	height: 100vh;
+	height: auto;
 	width: 300px;
-	background-color: #D7E3DE;
+	/* top: 0;
+	left: 0;
+	right: 0; */
+	background-color: #d7e3de;
+	/* position: fixed; */
 `;
 export const Logo = styled.img`
-cursor:pointer;
-	height:70px;
-	width:70px;
-	padding-top:10px;
-	padding-left:10px;
-`
+	cursor: pointer;
+	height: 70px;
+	width: 70px;
+	padding-top: 10px;
+	padding-left: 10px;
+`;
 export const MenuBar = styled.div`
-	margin:0 auto;
-	margin-top:50px;
-	width:60%;
-	height:350px;
-
-`
+	margin: 0 auto;
+	margin-top: 50px;
+	width: 60%;
+	height: 350px;
+`;
