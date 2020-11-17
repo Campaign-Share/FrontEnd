4194df9d92cd60c9faf7f69f8fc412e5064b16f6
Wanseo <sonwanseo@gmail.com>
2020-11-17 19:59:48 +0900

    create ViewInProgressContainer

---
+++ src/containers/MainContainer/ViewInProgressContainer/ViewInProgressContainer.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCampaignList } from '../../../modules/viewInProgress';
+import ViewInProgressModal from '../../../components/Modal/ViewInProgressModal/ViewInProgressModal';
+import ViewInProgress from '../../../components/ViewInProgress/ViewInProgress';
+import { requestApiWithAccessToken } from '../../../APIrequest';
+
+const ViewInProgressContainer = () => {
+	const dispatch = useDispatch();
+	const viewInProgressRedux = useSelector((state) => state.viewInProgress);
+
+	const getCampaign = async () => {
+		let count = 6,
+			start = viewInProgressRedux.index,
+			sortBy = viewInProgressRedux.sortBy;
+
+		const res = await requestApiWithAccessToken(
+			'/v1/campaigns/sorted-by/' +
+				sortBy +
+				'?start=' +
+				start +
+				'&count=' +
+				count +
+				'&state=approved',
+			{},
+			{},
+			'get',
+		);
+		dispatch(getCampaignList(res.data));
+	};
+
+	useEffect(() => {
+		try {
+			getCampaign();
+		} catch (err) {
+			console.error(err);
+		}
+	}, [viewInProgressRedux.sortBy]);
+
+	return (
+		<>
+			{viewInProgressRedux.onModal && <ViewInProgressModal />}
+			<ViewInProgress />
+		</>
+	);
+};
+
+export default ViewInProgressContainer;
