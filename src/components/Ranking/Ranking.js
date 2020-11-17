2444eeb27437bd63910b2a646647593fcbe27d3e
Haerin <kim34458747@gmail.com>
2020-11-18 01:13:09 +0900

    ranking 서버 통신

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,27 +1,68 @@
-import React from 'react';
-import SideBar from '../Navigation/sideBar/SideBar';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
-import { medal1, medal2, medal3 } from '../../assets/img/index';
+import { medal1, medal2, medal3, profile } from '../../assets/img/index';
+import CampaignSearchHeader from '../common/CampaignSearchHeader/CampaignSearchHeader';
+import { requestApiWithAccessToken } from '../../APIrequest';
 
 const Ranking = () => {
+	let [first, setFirst] = useState([]);
+	let [second, setSecond] = useState([]);
+	let [third, setThird] = useState([]);
+	let [data, setData] = useState({
+		frist: [],
+		second: [],
+		third: [],
+	});
+	let [isImg, setIsImg] = useState(false);
+	const imgUrl = 'https://campaignshare.s3.ap-northeast-2.amazonaws.com/';
+	useEffect(() => {
+		requestApiWithAccessToken(
+			`/v1/users/sorted-by/participate?start=0&count=3`,
+			{},
+			{},
+			'get',
+		).then((res) => {
+			setFirst(res.data.user_informs[0]);
+			setSecond(res.data.user_informs[1]);
+			setThird(res.data.user_informs[2]);
+			// setData(...data, data.first)
+		});
+	}, []);
+
+	// if (
+	// 	second.profile_uri === null ||
+	// 	first.profile_uri === null ||
+	// 	third.profile_uri === null
+	// ) {
+	// 	setIsImg(true);
+	// } else setIsImg(false);
+
+	console.log(second.campaign_number);
 	return (
 		<React.Fragment>
 			<S.RankingContainer>
+				<CampaignSearchHeader />
 				<S.ContentContainer>
 					<S.HeaderBox>이달의 명예의 전당</S.HeaderBox>
 					<S.UserContainer>
 						<S.SecondBox>
 							<S.Medal src={medal2} />
 							<S.InfoBox>
-								<S.UserImg />
-								<S.UserInfo>참여한 캠페인 : 15</S.UserInfo>
+								{isImg ? (
+									<S.UserImg src={imgUrl + second.profile_uri} />
+								) : (
+									<S.UserImg src={profile} />
+								)}
+								<S.User>{second.nick_name}</S.User>
+								<S.UserInfo>참여한 캠페인 : 10</S.UserInfo>
 								<S.UserInfo>등록한 캠페인 : 5</S.UserInfo>
 							</S.InfoBox>
 						</S.SecondBox>
 						<S.FirstBox>
 							<S.FirstMedal src={medal1} />
 							<S.InfoBox>
-								<S.FirstUser />
+								<S.FirstUser src={imgUrl + first.profile_uri} />
+								<S.User>{first.nick_name}</S.User>
 								<S.UserInfo>참여한 캠페인 : 20</S.UserInfo>
 								<S.UserInfo>등록한 캠페인 : 8</S.UserInfo>
 							</S.InfoBox>
@@ -29,7 +70,9 @@ const Ranking = () => {
 						<S.ThirdBox>
 							<S.Medal src={medal3} />
 							<S.InfoBox>
-								<S.ThirdUser />
+								{/* <S.ThirdUser src={imgUrl + second.profile_uri} /> */}
+								<S.ThirdUser src={profile} />
+								<S.User>{third.nick_name}</S.User>
 								<S.UserInfo>참여한 캠페인 : 8</S.UserInfo>
 								<S.UserInfo>등록한 캠페인 : 2</S.UserInfo>
 							</S.InfoBox>
