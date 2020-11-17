59b7c04901adfa978de23f2476d103f764d34664
Haerin <kim34458747@gmail.com>
2020-11-18 01:11:25 +0900

    mypage campaign list

--- src/containers/profileContainer/ListContainer/ListContainer.js
+++ src/containers/profileContainer/ListContainer/ListContainer.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import AcceptCampaign from '../../../components/Profile/mypage/CampaignList/AcceptCampaign';
-import EnterCampaign from '../../../components/Profile/mypage/CampaignList/EnterCampaign';
-import RefusalCampaign from '../../../components/Profile/mypage/CampaignList/RefusalCampaign';
-
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { requestApiWithAccessToken } from '../../../APIrequest';
+import MyCampaign from '../../../components/common/Campaign/MyCampaign';
+import CampaignsList from '../../../components/Profile/mypage/CampaignList/CampaignsList';
+import { campaignList } from '../../../modules/CampaignList';
 const ListContainer = (props) => {
 	const url = props.match.url;
+	const uuid = localStorage.getItem('user_uuid');
 	let list;
+	const dispatch = useDispatch();
+	const onList = () => {
+		dispatch(campaignList);
+	};
+	let [count, setCount] = useState(6);
+	let [loading, setLoading] = useState(false);
+
+	const onParticipationList = useCallback(() => {
+		requestApiWithAccessToken(
+			`/v1/users/uuid/${uuid}/campaigns?start=0&count=${count}&state=pending`,
+			{},
+			{},
+			'get',
+		).then((res) => {
+			dispatch(campaignList(res.data.campaigns));
+			console.log(res.data.campaigns);
+		});
+	});
+
+	const onAcceptList = useCallback(() => {
+		requestApiWithAccessToken(
+			`/v1/users/uuid/${uuid}/campaigns?start=0&count=${count}&state=approved`,
+			{},
+			{},
+			'get',
+		).then((res) => {
+			dispatch(campaignList(res.data.campaigns));
+		});
+	});
+
+	const onRefusalList = useCallback(() => {
+		requestApiWithAccessToken(
+			`/v1/users/uuid/${uuid}/campaigns?start=0&count=${count}&state=rejected`,
+			{},
+			{},
+			'get',
+		).then((res) => {
+			dispatch(campaignList(res.data.campaigns));
+		});
+	});
+
 	switch (url) {
-		case '/main/mypage/enterList': {
-			list = <EnterCampaign />;
+		case '/main/mypage/participationList': {
+			list = <CampaignsList />;
+			onParticipationList();
 			break;
 		}
 		case '/main/mypage/acceptList': {
-			list = <AcceptCampaign />;
+			list = <CampaignsList />;
+			onAcceptList();
 			break;
 		}
 		case '/main/mypage/refusalList': {
-			list = <RefusalCampaign />;
+			list = <CampaignsList />;
+			onRefusalList();
 			break;
 		}
 	}
+
 	return <React.Fragment>{list}</React.Fragment>;
 };
 export default ListContainer;
