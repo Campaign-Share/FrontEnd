b73e334da5a4cf92042e98daf9803f86cd473f46
Haerin <kim34458747@gmail.com>
2020-11-18 01:10:38 +0900

    campaign reducer index

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,12 +4,14 @@ import { loginReducer } from './Login';
 import { signUpReducer } from './SignUp';
 import editProfileReducer from './EditProfle';
 import editPasswordReducer from './EditPassword';
+import { campaignListReducer } from './CampaignList';
 const rootReducer = combineReducers({
 	mypage: mypageReducer,
 	editpage: editProfileReducer,
 	editPassword: editPasswordReducer,
 	signUpReducer,
 	loginReducer,
+	list: campaignListReducer,
 });
 
 export default rootReducer;
