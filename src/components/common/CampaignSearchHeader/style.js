dd3729df8ad8be27161af868195a052c275f24d8
Haerin <kim34458747@gmail.com>
2020-11-18 01:16:48 +0900

    header style 수정

--- src/components/common/CampaignSearchHeader/style.js
+++ src/components/common/CampaignSearchHeader/style.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export const Header = styled.div`
-	width: 80%;
+	width: 100%;
 	height: 4.375rem;
 	display: flex;
 	justify-content: flex-end;
 	align-items: center;
+	margin-bottom: 20px;
 `;
 
 export const SearchBarWrapper = styled.div`
