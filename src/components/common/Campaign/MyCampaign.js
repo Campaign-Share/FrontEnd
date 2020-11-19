8a22f1e4779275e6da6aec66446fb0dbfa40d850
Haerin <kim34458747@gmail.com>
2020-11-19 13:26:29 +0900

    mycampaign 수정

--- src/components/common/Campaign/MyCampaign.js
+++ src/components/common/Campaign/MyCampaign.js
@@ -2,52 +2,51 @@ import React, { useState } from 'react';
 import * as S from './style';
 import { good, bad, member } from '../../../assets/img/index';
 
-const MyCampaign = ({ props, onClick }) => {
+const MyCampaign = ({ props, onClick, isSuggested }) => {
 	const imgUrl = 'https://campaignshare.s3.ap-northeast-2.amazonaws.com/';
-	const text = props.user_uuid;
-	const tags = props.campaign_tags;
-	const number = props.agree_number;
-	const campaign = props.campaign_uuid;
-	let [isGraph, setIsGraph] = useState(true);
-	// if (number == 0) {
-	// 	setIsGraph(false);
-	// } else setIsGraph(true);
-
+	console.log(isSuggested);
 	return (
 		<S.MainWrapper onClick={onClick}>
 			<S.Poster src={imgUrl + props.post_uri} />
 			<S.CampaignTextWrapper>
-				<S.CampaignPublisher text={text.length}>{text}</S.CampaignPublisher>
+				<S.CampaignPublisher>{props.user_uuid}</S.CampaignPublisher>
 				<S.CampaignSummaryWrapper>
 					<S.CampaignSubtitle>{props.sub_title}</S.CampaignSubtitle>
 					<S.CampaignTitle>{props.title}</S.CampaignTitle>
 					{/* {tags.map((tag) => (
-						<S.CampaignTags>{tag}</S.CampaignTags>
+						<S.CampaignTags>{tag.replace(/'/g, '')}</S.CampaignTags>
 					))} */}
-					<S.CampaignTags>{tags}</S.CampaignTags>
+					<S.CampaignTags>{props.tags}</S.CampaignTags>
 				</S.CampaignSummaryWrapper>
 			</S.CampaignTextWrapper>
-			{isGraph ? (
+			{isSuggested ? (
 				<S.CampaignGraphWrapper>
 					<S.CampaignLikeIcon src={good} />
 					<S.CampaignDisagreeGraph>
-						<S.CampaignFlexDiv>
+						<S.CampaignFlexDiv
+							flex={props.agree_number}
+							isFull={props.disagree_number === 0}>
 							<S.CampaignAgreeNumber>
 								{props.agree_number}
 							</S.CampaignAgreeNumber>
 						</S.CampaignFlexDiv>
-						<S.CampaignFlexDiv>
+						<S.CampaignFlexDiv flex={props.disagree_number}>
 							<S.CampaignDisagreeNumber>
 								{props.disagree_number}
 							</S.CampaignDisagreeNumber>
 						</S.CampaignFlexDiv>
 					</S.CampaignDisagreeGraph>
+					<S.CampaignDislikeIcon src={bad} />
 				</S.CampaignGraphWrapper>
 			) : (
-				<S.CampaignMember>
-					<S.Member src={member} />
-					{props.participation_number}
-				</S.CampaignMember>
+				<S.CampaignParticipationWrapper>
+					<S.CampaignParticipationDiv>
+						<S.CampaignParticipationIcon src={member} />
+						<S.CampaignParticipationText>
+							{props.participation_number}
+						</S.CampaignParticipationText>
+					</S.CampaignParticipationDiv>
+				</S.CampaignParticipationWrapper>
 			)}
 		</S.MainWrapper>
 	);
