c8a21d2ee613ceba775c92a2c7b255bda4775d0f
Wanseo <sonwanseo@gmail.com>
2020-11-19 11:22:59 +0900

    InProgressJoinContainer

---
+++ src/containers/MainContainer/InProgressJoinContainer/InProgressJoinContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import InProgressJoin from '../../../components/InProgressJoin/InProgressJoin';
+
+const InProgressJoinContainer = () => {
+	return <InProgressJoin />;
+};
+
+export default InProgressJoinContainer;
