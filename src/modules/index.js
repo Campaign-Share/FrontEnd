735b45fb1dc3c4d2e323d1194ce89d374310f659
김해린 <rin3583@naver.com>
2020-12-03 00:19:15 +0900

    module edit

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,7 +4,6 @@ import { loginReducer } from './Login';
 import { signUpReducer } from './SignUp';
 import editProfileReducer from './EditProfle';
 import editPasswordReducer from './EditPassword';
-import { campaignListReducer } from './CampaignList';
 import viewInProgressReducer from './viewInProgress';
 import viewSuggestedReducer from './viewSuggested';
 import reportModalReducer from './reportModal';
@@ -20,7 +19,6 @@ const rootReducer = combineReducers({
 	inProgressJoin: inProgressJoinReducer,
 	signUpReducer,
 	loginReducer,
-	list: campaignListReducer,
 });
 
 export default rootReducer;
