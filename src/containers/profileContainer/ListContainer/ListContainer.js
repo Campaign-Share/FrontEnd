53a678be9641f3b74759beb7340872211591da30
김해린 <rin3583@naver.com>
2020-12-04 09:24:36 +0900

    campaign list edit

--- src/containers/profileContainer/ListContainer/ListContainer.js
+++ src/containers/profileContainer/ListContainer/ListContainer.js
@@ -1,13 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { requestApiWithAccessToken } from '../../../APIrequest';
-import ViewInProgressModal from '../../../components/Modal/ViewInProgressModal/ViewInProgressModal';
+import MyCampaign from '../../../components/common/Campaign/MyCampaign';
 import CampaignsList from '../../../components/Profile/mypage/CampaignList/CampaignsList';
 import {
 	campaignList,
 	campaignOff,
 	campaignOn,
 } from '../../../modules/CampaignList';
+
 const ListContainer = (props) => {
 	const url = props.match.url;
 	const uuid = localStorage.getItem('user_uuid');
@@ -15,20 +16,18 @@ const ListContainer = (props) => {
 	let [count, setCount] = useState(6);
 	let [loading, setLoading] = useState(false);
 	let [component, setComponent] = useState(<></>);
-	const viewModal = useSelector((state) => state.viewInProgress);
 
 	const onParticipationList = useCallback(() => {
 		requestApiWithAccessToken(
-			`/v1/users/uuid/${uuid}/campaigns?start=0&count=${count}&state=pending`,
+			`/v1/users/uuid/${uuid}/participate-campaigns?start=0&count=${count}`,
+
 			{},
 			{},
 			'get',
 		).then((res) => {
 			console.log(res.data);
-			if (res.data.campaigns.length == 0) {
-				dispatch(campaignOff());
-				setLoading(false);
-			} else {
+			if (res.data.campaigns.length == 0) dispatch(campaignOff());
+			else {
 				dispatch(campaignList(res.data.campaigns));
 				dispatch(campaignOn());
 				setLoading(true);
@@ -43,10 +42,8 @@ const ListContainer = (props) => {
 			{},
 			'get',
 		).then((res) => {
-			if (res.data.campaigns.length == 0) {
-				dispatch(campaignOff());
-				setLoading(false);
-			} else {
+			if (res.data.campaigns.length == 0) dispatch(campaignOff());
+			else {
 				dispatch(campaignList(res.data.campaigns));
 				dispatch(campaignOn());
 				setLoading(true);
@@ -62,10 +59,8 @@ const ListContainer = (props) => {
 			{},
 			'get',
 		).then((res) => {
-			if (res.data.campaigns.length == 0) {
-				dispatch(campaignOff());
-				setLoading(false);
-			} else {
+			if (res.data.campaigns.length == 0) dispatch(campaignOff());
+			else {
 				dispatch(campaignList(res.data.campaigns));
 				dispatch(campaignOn());
 				setLoading(true);
@@ -101,18 +96,16 @@ const ListContainer = (props) => {
 	useEffect(() => {
 		if (url === '/main/mypage/participationList') {
 			onParticipationList();
-			viewModal.onModal && <ViewInProgressModal />;
 			setComponent(<CampaignsList isSuggested={false} isSelect={1} />);
 		} else if (url === '/main/mypage/acceptList') {
 			onAcceptList();
-			viewModal.onModal && <ViewInProgressModal />;
 			setComponent(<CampaignsList isSuggested={true} isSelect={2} />);
 		} else if (url === '/main/mypage/refusalList') {
 			onRefusalList();
-			viewModal.onModal && <ViewInProgressModal />;
 			setComponent(<CampaignsList isSuggested={false} isSelect={3} />);
 		} else 0;
 	}, [url]);
+
 	return <React.Fragment>{component}</React.Fragment>;
 };
 export default ListContainer;
