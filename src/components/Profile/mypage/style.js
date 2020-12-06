d0b21ebbbda90986e90eda1cf41464c6f0158253
김해린 <rin3583@naver.com>
2020-12-06 22:15:21 +0900

    mypage style edit

--- src/components/Profile/mypage/style.js
+++ src/components/Profile/mypage/style.js
@@ -7,11 +7,11 @@ export const Container = styled.div`
 `;
 export const Body = styled.div`
 	flex: 1;
-	padding-left: 300px;
+	padding-left: 18.75rem;
 `;
 export const HeaderEnd = styled.div`
-	margin-top: 30px;
-	margin-right: 50px;
+	margin-top: 1.875rem;
+	margin-right: 3.125rem;
 	display: flex;
 	justify-content: flex-end;
 `;
@@ -19,8 +19,8 @@ export const EditProfileBtn = styled.button`
 	display: block;
 	background-color: #c1dcdb;
 	color: white;
-	width: 120px;
-	height: 40px;
+	width: 7.5rem;
+	height: 2.5rem;
 	cursor: pointer;
 	border: solid;
 	border-radius: 2rem;
@@ -29,29 +29,29 @@ export const EditProfileBtn = styled.button`
 export const MainInfo = styled.div`
 	width: 100%;
 	height: 70%;
-	margin-top: 50px;
+	margin-top: 3.125rem;
 	display: flex;
 `;
 export const UserdataBox = styled.div`
 	height: 100%;
 	width: 80%;
 
-	margin: 40px;
+	margin: 2.5rem;
 	border: 1px solid #e7e7e7;
 	border-radius: 2rem;
 	box-shadow: 1px 3px #e7e7e7;
 `;
 export const UserImgBox = styled.div`
 	display: flex;
-	height: 160px;
+	height: 10rem;
 	width: 100%;
 	justify-content: center;
 	align-items: center;
 `;
 export const UserImg = styled.img`
 	border-radius: 10rem;
-	width: 110px;
-	height: 110px;
+	width: 6.875rem;
+	height: 6.875rem;
 `;
 export const UserId = styled.div`
 	width: 100%;
@@ -59,14 +59,14 @@ export const UserId = styled.div`
 	color: #8a8a8a;
 `;
 export const UserName = styled.div`
-	margin-top: 15px;
+	margin-top: 0.9375rem;
 	width: 100%;
 	text-align: center;
-	font-size: 15px;
+	font-size: 0.9375rem;
 `;
 export const MarginBox = styled.div`
-	height: 280px;
-	width: 200px;
+	height: 17.5rem;
+	width: 12.5rem;
 `;
 export const Logout = styled.div`
 	color: red;
