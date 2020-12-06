2f85d7803e34dec24e57e621b5c1ac9008c20c9c
김해린 <rin3583@naver.com>
2020-12-06 22:15:13 +0900

    campaign style edit

--- src/components/Profile/mypage/userCampaignInfo/style.js
+++ src/components/Profile/mypage/userCampaignInfo/style.js
@@ -19,40 +19,39 @@ export const UserCampaign = styled.div`
 `;
 export const GraphBox = styled.div`
 	flex: 1;
-	margin: 40px;
+	margin: 2.5rem;
 `;
 export const StickBox = styled.div`
 	display: flex;
-	height: 350px;
+	height: 21.88rem;
 	width: 100%;
 	justify-content: space-around;
-	align-items:flex-end;
+	align-items: flex-end;
 `;
 export const Stick = styled.div`
-	max-height:300px;
-	height:0px;
-	transition:height 1s;
+	max-height: 18.75rem;
+	height: 0;
+	transition: height 1s;
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-end;
-	height: ${props => props.height}px;
+	height: ${(props) => props.height}px;
 	width: 20%;
-	background-color: ${props=>props.color};
+	background-color: ${(props) => props.color};
 	border-radius: 1rem 1rem 0 0;
 `;
 export const Line = styled.div`
 	background-color: #7b7b7b;
-	border-radius: 5px;
-	height: 1px;
+	border-radius: 0.3125rem;
+	height: 0.0625rem;
 	width: 100%;
 `;
 export const NumberBox = styled.div`
-	display:flex;
+	display: flex;
 	justify-content: space-around;
-	width:100%;
-	margin-top:10px;	
+	width: 100%;
+	margin-top: 0.625rem;
 `;
 export const Number = styled.div`
-	text-align:center;
-	
+	text-align: center;
 `;
