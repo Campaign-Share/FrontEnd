b7cf1e5829bff375a097cbc632587dc107c98178
김해린 <rin3583@naver.com>
2020-12-06 22:15:34 +0900

    campaignList style edit

--- src/components/Profile/mypage/CampaignList/style.js
+++ src/components/Profile/mypage/CampaignList/style.js
@@ -2,17 +2,17 @@ import styled, { css } from 'styled-components';
 
 export const MainContainer = styled.div`
 	display: flex;
-	width: calc(100% - 300px);
+	width: calc(100% - 18.75rem);
 	justify-content: space;
 	flex-wrap: wrap;
 	flex: 1;
-	padding-left: 300px;
+	padding-left: 18.75rem;
 	height: 100%;
 `;
 
 export const ListContainer = styled.div`
 	width: 100%;
-	margin: 10px 0px 0px 100px;
+	margin: 0.625rem 0 0 6.25rem;
 	justify-content: space;
 	flex-wrap: wrap;
 	display: flex;
@@ -20,12 +20,11 @@ export const ListContainer = styled.div`
 
 export const HeaderBox = styled.div`
 	width: 100%;
-	margin-bottom: 20px;
+	margin-bottom: 1.25rem;
 `;
 
 export const Filtering = styled.a`
-	width: 140px;
-	margin-right: 30px;
+	margin-right: 1.875rem;
 	border-bottom: 3px solid ${(props) => props.id == props.select && '#9dc3c1'};
 	color: ${(props) =>
 		props.id == props.select || props.id == props.pick ? '#9dc3c1' : '#535353'};
